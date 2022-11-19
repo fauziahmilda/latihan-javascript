2d32700788d58cbe37f6ae3f50207c67011c64b0
fauziahmilda <fauziah.milda@gmail.com>
2022-11-19 16:30:43 +0700

    add rest parameter in spread operator

--- spread-operator/script.js
+++ spread-operator/script.js
@@ -17,8 +17,30 @@ console.log(nVal);
 const val = user[0].toUpperCase();
 console.log(val);
 
-//------------------------------------
+//------------------------------------------tampil
 const nama = document.querySelector(".nama");
 const huruf = [...nama.textContent].map((h) => `<span>${h}</span>`).join("");
 nama.innerHTML = huruf;
 console.log(huruf);
+
+//------------------------------------------
+function myFunc(...myArgs) {
+  return myArgs;
+  //atau
+  // return Array.from(arguments);
+  // return [...arguments];
+}
+console.log(myFunc(1, 2, 3, 4, 5));
+//------------------------------------------
+function jumlah(...angka) {
+  let total = 0;
+  for (const a of angka) {
+    //a itu satu dari angka
+    total += a;
+  }
+  return total;
+
+  //atau
+  // return angka.reduce((a,b)=> a+b);
+}
+console.log(jumlah(1, 2, 3, 4, 5));
